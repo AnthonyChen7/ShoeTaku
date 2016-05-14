97aaee55e2b1023f62b032292eea3e91cd9056db
Edward Damwon Choi <edwardchoi90@gmail.com>
2016-05-13 20:55:34 -0700

    edited sql

--- controllers/shoe.php
+++ controllers/shoe.php
@@ -17,23 +17,10 @@ class Shoe extends Restapi
 		$method = $_SERVER['REQUEST_METHOD'];
 		$requestArray = explode("/", $_REQUEST['x']);
 		$length = count($requestArray);
-		$table = "Shoe";
-		$brand = $_POST["brand"];
-		$model = $_POST["model"];
-		$size = $_POST["size"];
-		$itemCnd = $_POST["itemCondition"];
-		$description = $_POST["description"];
-		$imageUrl = $_POST["imageUrl"];
-		$ownerId = $_POST["ownerId"];
-
-		$columns = array("brand", "model", "size", "itemCondition", "description", "imageUrl", "ownerId");
-		$values = array($brand, $model, $size, $itemCnd, $description, $imageUrl, $ownerId);
 
 		$sql = $this->prepareInsertSql($table, $columns);
-		echo($sql);
 		
 		if ($method == 'POST'){
-			//$json = file_get_contents("php://input");
 			
 			// Base case: /controllers/shoe   Create a new Shoe
 			if ($length == 1)
@@ -86,14 +73,13 @@ class Shoe extends Restapi
 		$itemCnd = $_POST["itemCondition"];
 		$description = $_POST["description"];
 		$imageUrl = $_POST["imageUrl"];
-		$ownerId = $_POST["ownerId"];
+		$ownerId = 1;//$_POST["ownerId"];
+		$isWanted = $_POST["isWanted"];
 
-		$columns = array("brand", "model", "size", "itemCondition", "description", "imageUrl", "ownerId");
-		$values = array($brand, $model, $size, $itemCnd, $description, $imageUrl, $ownerId);
+		$columns = array("brand", "model", "size", "itemCondition", "description", "imageUrl", "ownerId", "isWanted");
+		$values = array($brand, $model, $size, $itemCnd, $description, $imageUrl, $ownerId, $isWanted);
 
 		$sql = $this->prepareInsertSql($table, $columns);
-		print($sql);
-		echo($sql);
 
 		try
 		{
