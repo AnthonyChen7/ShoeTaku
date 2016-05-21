bdd68d5a4a54eed6fc901337607e3721f02edb44
peterysc <peterysc@gmail.com>
2016-05-20 20:55:07 -0700

    temp commit

--- controllers/shoe.php
+++ controllers/shoe.php
@@ -17,17 +17,16 @@ class Shoe extends Restapi
 		$method = $_SERVER['REQUEST_METHOD'];
 		$requestArray = explode("/", $_REQUEST['x']);
 		$length = count($requestArray);
-		$this->response("test",200);
 		
 		if ($method == 'POST'){
 			// Base case: /controllers/shoe   Create a new Shoe
 			if ($length == 1){
-				$this->response("test",200);
 				if(isset($_POST['page'])){
 					$result = $this->paginationShoe();
 				}else{
-			 		echo "<script>alert('1234');</script>";
+			 		$result = $this->createShoe();
 				}
+				$this->response($result, 200);
 			}
 			if ($length == 2){
 				$id = $requestArray[1];
@@ -41,8 +40,9 @@ class Shoe extends Restapi
 			if ($length == 1) $this->getShoes();
 			if ($length == 2){
 				$id = $requestArray[1];
-				if (is_int($id) && $id >= 0) 
+				if (is_int($id) && $id >= 0){
 					$result = $this->getShoe($id);
+				}
 			}
 		}
 
@@ -69,8 +69,8 @@ class Shoe extends Restapi
 
 	private function paginationShoe()
 	{
-		$page = $_POST['page']; // Current page number
-		$per_page = $_POST['per_page']; // Articles per page
+		$page = $_POST["page"]; // Current page number
+		$per_page = $_POST["per_page"]; // Articles per page
 		if ($page != 1) 
 		{
 			$start = ($page-1) * $per_page;
@@ -87,74 +87,74 @@ class Shoe extends Restapi
 		return $result;
 	}
 
-	// private function createShoe()
-	// {
-	// 	$table = "Shoe";
-	// 	$brand = $_POST["brand"];
-	// 	$model = $_POST["model"];
-	// 	$size = $_POST["size"];
-	// 	$itemCnd = $_POST["itemCondition"];
-	// 	$description = $_POST["description"];
-	// 	$ownerId = 0;//$_POST["ownerId"];
-	// 	$isWanted = $_POST["isWanted"];
-	// 	$url;
+	private function createShoe()
+	{
+		$table = "Shoe";
+		$brand = $_POST["brand"];
+		$model = $_POST["model"];
+		$size = $_POST["size"];
+		$itemCnd = $_POST["itemCondition"];
+		$description = $_POST["description"];
+		$ownerId = 0;//$_POST["ownerId"];
+		$isWanted = $_POST["isWanted"];
+		$url;
 		
-	// 	if(isset($_POST["image"]))
-	// 	{
-	// 		$img = $_POST["image"];
+		if(isset($_POST["image"]))
+		{
+			$img = $_POST["image"];
 
-	// 		if (isset($_POST['submit'])){ 
-	// 			if($img['name']==''){  
-	// 				echo "<h2>An Image Please.</h2>";
-	// 			}else{
-	// 				$filename = $img['tmp_name'];
-	// 				$client_id = /*imgur client id*/; 
-	// 				$handle = fopen($filename, "r");
-	// 				$data = fread($handle, filesize($filename));
-	// 				$pvars   = array('image' => base64_encode($data));
-	// 				$timeout = 30;
+			if (isset($_POST['submit'])){ 
+				if($img['name']==''){  
+					echo "<h2>An Image Please.</h2>";
+				}else{
+					$filename = $img['tmp_name'];
+					// $client_id = imgur client id; 
+					$handle = fopen($filename, "r");
+					$data = fread($handle, filesize($filename));
+					$pvars   = array('image' => base64_encode($data));
+					$timeout = 30;
 					
-	// 				$curl = curl_init();
-	// 				curl_setopt($curl, CURLOPT_URL, 'https://api.imgur.com/3/image.json');
-	// 				curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
-	// 				curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Client-ID ' . $client_id));
-	// 				curl_setopt($curl, CURLOPT_POST, 1);
-	// 				curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-	// 				curl_setopt($curl, CURLOPT_POSTFIELDS, $pvars);
-	// 				$out = curl_exec($curl);
-	// 				curl_close ($curl);
-	// 				$pms = json_decode($out, true);
-	// 				$url = $pms['data']['link'];
-	// 		 	}
-	// 		}
-	// 	}else if(isset($_POST["url"])){
-	// 		$url = $_POST["url"];
+					$curl = curl_init();
+					curl_setopt($curl, CURLOPT_URL, 'https://api.imgur.com/3/image.json');
+					curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
+					curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Client-ID ' . $client_id));
+					curl_setopt($curl, CURLOPT_POST, 1);
+					curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+					curl_setopt($curl, CURLOPT_POSTFIELDS, $pvars);
+					$out = curl_exec($curl);
+					curl_close ($curl);
+					$pms = json_decode($out, true);
+					$url = $pms['data']['link'];
+			 	}
+			}
+		}else if(isset($_POST["url"])){
+			$url = $_POST["url"];
 
-	// 	}else{
-	// 		// No $url is set
-	// 		// error!!!
-	// 	}
+		}else{
+			// No $url is set
+			// error!!!
+		}
 
-	// 	$columns = array("brand", "model", "size", "itemCondition", "description", "imageUrl", "ownerId", "isWanted");
-	// 	$values = array($brand, $model, $size, $itemCnd, $description, $url, $ownerId, $isWanted);
-	// 	$sql = $this->prepareInsertSql($table, $columns);
+		$columns = array("brand", "model", "size", "itemCondition", "description", "imageUrl", "ownerId", "isWanted");
+		$values = array($brand, $model, $size, $itemCnd, $description, $url, $ownerId, $isWanted);
+		$sql = $this->prepareInsertSql($table, $columns);
 
-	// 	try
-	// 	{
-	// 		$this->connect();
-	// 		$stmt = $this->conn->prepare($sql);
-	// 		$result = $stmt->execute($values);
+		try
+		{
+			$this->connect();
+			$stmt = $this->conn->prepare($sql);
+			$result = $stmt->execute($values);
 
-	// 		$this->redirect($_SERVER['SERVER_NAME']);
+			$this->redirect($_SERVER['SERVER_NAME']);
 
-	// 	} catch (Exception $e) {
-	// 		$result = FALSE;
-	// 		//c$this->redirect($_SERVER['SERVER_NAME']);
-	// 	}
-	// 	$this->disconnect();
+		} catch (Exception $e) {
+			$result = FALSE;
+			//c$this->redirect($_SERVER['SERVER_NAME']);
+		}
+		$this->disconnect();
 
-	// 	return $result;
-	// }
+		return $result;
+	}
 
 	private function editShoe($id)
 	{
