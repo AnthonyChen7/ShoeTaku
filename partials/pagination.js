ec3fe913278897fdf0b050b3256f65b57dcd92ef
peterysc <peterysc@gmail.com>
2016-05-23 17:50:08 -0700

    sell.html now displays 9 posts

--- partials/pagination.js
+++ partials/pagination.js
@@ -14,9 +14,9 @@ $('#sellPage').on('click', function(e) { // When click on a 'a' element of the p
 	var pagination = ''; // Init pagination
 	var url = "/controllers/shoe";
 	
-	var data = {page: page, per_page: 3}; // Create JSON which will be sent via Ajax
+	var data = {page: page, per_page: 9}; // Create JSON which will be sent via Ajax
 	// We set up the per_page var at 4. You may change to any number you need.
-	alert(data);
+
 	$.ajax({ // jQuery Ajax
 		type: 'POST',
 		url: url, // URL to the PHP file which will insert new value in the database
@@ -24,16 +24,23 @@ $('#sellPage').on('click', function(e) { // When click on a 'a' element of the p
 		dataType: 'json', // Json format
 		timeout: 3000,
 		success: function(data) {
-			alert("success");
-			console.log(data);
-			console.log(data[0]['brand']);
-			console.log(data[1]['brand']);
-			console.log(data[2]['brand']);
+			// TODO: figure out to implement this using loop
+			$('#first_firstPost').html(data[0]['brand']);
+			$('#first_secondPost').html(data[1]['brand']);
+			$('#first_thirdPost').html(data[2]['brand']);
+			$('#second_firstPost').html(data[3]['brand']);
+			$('#second_secondPost').html(data[4]['brand']);
+			$('#second_thirdPost').html(data[5]['brand']);
+			$('#third_firstPost').html(data[6]['brand']);
+			$('#third_secondPost').html(data[7]['brand']);
+			$('#third_thirdPost').html(data[8]['brand']);
 
 		},
 		error: function(data) {
-			alert("123");
+			// for debugging purpose
+			alert("error");
 		}
 	});
 	return false;
 });
+
