0fa96a564e80906520cdf3d5c4099fba2cd1195c
someHobo <xwei7778@gmail.com>
2016-06-10 15:22:48 -0700

    Got ajax call working to php file

---
+++ controllers/accountSettings.php
@@ -0,0 +1,18 @@
+<?php 
+
+require_once(__DIR__.'/restapi.php');
+
+define("RANDOM_STRING", '70bpyytrEVHXNC99PvjKfNcgHLwByB2B9eGExqiBYSG6LdnjdT2q9nARwCKWVNy');
+
+class AccountSettings extends Restapi{
+	
+	function __construct(){
+		parent::__construct();
+		
+		$method = $_SERVER['REQUEST_METHOD'];
+		
+		echo $method;
+	}
+}
+
+?>
--- partials/account.js
+++ partials/account.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
-    // console.log(localStorage.getItem("token"));
     
     //make ajax call to populate fields in account.html
+    
+    $.ajax(
+    {
+        type: 'GET',
+        url:"/controllers/accountSettings",
+        data: localStorage.getItem("token"),
+        success: function(data){
+           console.log(data); 
+        },
+        error: function(data){
+            
+        }
+        
+    }
+    );
+    
 });
