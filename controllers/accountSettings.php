07e756f2eb6d91b6a3aff839bbd9ea994b367be1
someHobo <xwei7778@gmail.com>
2016-06-10 17:15:22 -0700

    Able to validate and verify token

--- controllers/accountSettings.php
+++ controllers/accountSettings.php
@@ -1,8 +1,15 @@
 <?php 
 
+
+require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . '\vendor\autoload.php' );
 require_once(__DIR__.'/restapi.php');
 
 define("RANDOM_STRING", '70bpyytrEVHXNC99PvjKfNcgHLwByB2B9eGExqiBYSG6LdnjdT2q9nARwCKWVNy');
+define("ISSUER", 'ShoeTaku');
+
+use Lcobucci\JWT\ValidationData;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+use Lcobucci\JWT\Parser;
 
 class AccountSettings extends Restapi{
 	
@@ -11,7 +18,29 @@ class AccountSettings extends Restapi{
 		
 		$method = $_SERVER['REQUEST_METHOD'];
 		
-		echo $_GET["token"];
+		if($method == 'GET'){
+			$this->retrieveInfo();
+		}
+	}
+	
+	function retrieveInfo(){
+		$token = $_GET["token"];
+		
+		//parse the token
+		$token = (new Parser())->parse((string)$token);
+		
+		//validate & verify token first...
+		$signer = new Sha256();
+		
+		$data = new ValidationData();
+		$data->setIssuer(ISSUER);
+		//$data->setId($token->getClaim('jti'));
+		
+		if($token->verify($signer,RANDOM_STRING) && $token->validate($data)){
+			
+			echo "true";
+			
+		}
 	}
 }
 
