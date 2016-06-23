db8515983af53d1be7dce36593d6bf417c0e218f
someHobo <xwei7778@gmail.com>
2016-06-22 21:18:31 -0700

    Deleted old code in register.php for manually creating tokens.

--- controllers/register.php
+++ controllers/register.php
@@ -7,14 +7,7 @@ This class handles the non-FB authentication
 */
 
 require_once(__DIR__.'/restapi.php');
-
-use Lcobucci\JWT\Builder;
-use Lcobucci\JWT\Signer\Hmac\Sha256;
-
-define("USE_TIME", 60);
-define("EXPIRATION_TIME", 3600);
-define("RANDOM_STRING", '70bpyytrEVHXNC99PvjKfNcgHLwByB2B9eGExqiBYSG6LdnjdT2q9nARwCKWVNy');
-define("ISSUER", 'ShoeTaku');
+include_once __DIR__.'/tokencreator.php';
 
 class Register extends Restapi{
 	
@@ -25,12 +18,9 @@ class Register extends Restapi{
 	}
 	
 	private function register(){
-		// array to pass back data
-		//store token inside this array
-		//$data = array();
+
 		$token = NULL;
-		$signer = new Sha256();
-		
+
 		$result = NULL;
 				
 		$email = $_POST["email"];
@@ -72,17 +62,8 @@ class Register extends Restapi{
 		$result = $stmt->fetchAll();
 		$result = $result[0];
 		
-		// $data["success"] = true;
-		$token = (new Builder())
-						->setIssuer(ISSUER) // Configures the issuer (iss claim)
-                        //->setAudience('http://example.org') // Configures the audience (aud claim)
-                        ->setId($result['userId'], true) // Configures the id (jti claim), replicating as a header item
-                        ->setIssuedAt(time()) // Configures the time that the token was issue (iat claim)
-                        //->setNotBefore(time() + USE_TIME) // Configures the time that the token can be used (nbf claim)
-                        ->setExpiration(time() + EXPIRATION_TIME) // Configures the expiration time of the token (nbf claim)
-                        //->set('userId', $result['userId']) // Configures a new claim, called "uid"
-						->sign($signer, RANDOM_STRING) // creates a signature using "testing" as key
-                        ->getToken(); // Retrieves the generated token
+		$tokenCreator = new TokenCreator($result['userId']);
+		$token = $tokenCreator->getToken();
 		
 		}catch (Exception $e) {
 			$token = "error";
