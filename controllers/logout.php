8458c6880167164b3196938c1df2db57540f9e6c
someHobo <xwei7778@gmail.com>
2016-06-24 17:53:35 -0700

    Created skeleton file to handle storing the invalid db on logout

---
+++ controllers/logout.php
@@ -0,0 +1,36 @@
+<?php 
+//stores invalid token in db on logout
+require_once(__DIR__.'/restapi.php');
+
+class Logout extends Restapi{
+	
+	function __construct(){
+		parent::_construct();
+		$this->storeTokenInDB();
+	}
+	
+	/**
+	Token is considered invalid when user logs out
+	Store it in db
+	*/
+	private function storeTokenInDB(){
+		$table="invalid_token";
+		$columns=array("tokenId","expiry_time");
+		$values=array();
+		$limOff = array();
+		$where=array();
+		
+		$sql = $this->prepareInsertSql($table,$columns,$where,$limOff);
+		
+		try{
+			$this->connect();
+			
+			//TODO
+			
+			$this->disconnect();
+		}
+	}
+	
+}
+
+?>
