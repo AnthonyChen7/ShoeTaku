92cf47d0f3c8dbdc001b41400a19945670f6902b
someHobo <xwei7778@gmail.com>
2016-06-28 17:52:32 -0700

    Commented out insert statements cuz they r causing issues

--- sql/seed.sql
+++ sql/seed.sql
@@ -43,18 +43,18 @@ isWanted TINYINT NOT NULL DEFAULT 0,
 FOREIGN KEY (ownerId) REFERENCES USER (userId) ON DELETE CASCADE
 );
 
-INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike", "NikeShoes", 9, 1,"Mint", "example.com", 1, 0, 1);
-INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike2", "NikeShoes", 10, 1,"Mint", "example.com", 1, 0, 1);
-INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike3", "NikeShoes", 8, 1,"Mint", "example.com", 1, 0, 1);
-INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike4", "NikeShoes", 11, 1,"Mint", "example.com", 1, 0, 1);
-INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike5", "NikeShoes", 12, 1,"Mint", "example.com", 1, 0, 1);
-INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike6", "NikeShoes", 7, 1,"Mint", "example.com", 1, 0, 1);
-INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike7", "NikeShoes", 9, 1,"Mint", "example.com", 1, 0, 1);
-INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike8", "NikeShoes", 10, 1,"Mint", "example.com", 1, 0, 1);
-INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike9", "NikeShoes", 8, 1,"Mint", "example.com", 1, 0, 1);
-INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike10", "NikeShoes", 11, 1,"Mint", "example.com", 1, 0, 1);
-INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike11", "NikeShoes", 12, 1,"Mint", "example.com", 1, 0, 1);
-INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike12", "NikeShoes", 7, 1,"Mint", "example.com", 1, 0, 1);
+-- INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike", "NikeShoes", 9, 1,"Mint", "example.com", 1, 0, 1);
+-- INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike2", "NikeShoes", 10, 1,"Mint", "example.com", 1, 0, 1);
+-- INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike3", "NikeShoes", 8, 1,"Mint", "example.com", 1, 0, 1);
+-- INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike4", "NikeShoes", 11, 1,"Mint", "example.com", 1, 0, 1);
+-- INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike5", "NikeShoes", 12, 1,"Mint", "example.com", 1, 0, 1);
+-- INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike6", "NikeShoes", 7, 1,"Mint", "example.com", 1, 0, 1);
+-- INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike7", "NikeShoes", 9, 1,"Mint", "example.com", 1, 0, 1);
+-- INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike8", "NikeShoes", 10, 1,"Mint", "example.com", 1, 0, 1);
+-- INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike9", "NikeShoes", 8, 1,"Mint", "example.com", 1, 0, 1);
+-- INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike10", "NikeShoes", 11, 1,"Mint", "example.com", 1, 0, 1);
+-- INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike11", "NikeShoes", 12, 1,"Mint", "example.com", 1, 0, 1);
+-- INSERT INTO Shoe(brand, model, size, itemCondition, description, imageUrl, ownerId, likeCount, isWanted) VALUES ("Nike12", "NikeShoes", 7, 1,"Mint", "example.com", 1, 0, 1);
 
 CREATE TABLE Wanted
 (
