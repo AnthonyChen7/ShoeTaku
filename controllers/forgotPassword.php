1464272c0af5a317c8fca525c6fc1d7e7fb9dbf9
someHobo <xwei7778@gmail.com>
2016-07-21 18:13:47 -0700

    Temp commit

--- controllers/forgotPassword.php
+++ controllers/forgotPassword.php
@@ -1,6 +1,10 @@
-<?php 
+<?php
+require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . '\vendor\autoload.php' ); 
+
 require_once(__DIR__.'/restapi.php');
-include_once (__DIR__.'/validate.php');
+require_once(__DIR__.'/validate.php');
+
+require(realpath($_SERVER["DOCUMENT_ROOT"]) . '\vendor\phpmailer\phpmailer\PHPMailerAutoload.php' );
 
 class ForgotPassword extends Restapi{
 	
@@ -16,7 +20,52 @@ class ForgotPassword extends Restapi{
 	}
 	
 	private function forgotPassword(){
-		return true;
+		// $mail = new PHPMailer;
+		
+		// //Set PHPMailer to use the sendmail transport
+		// $mail->isSendmail();
+		
+		// $mail->IsSMTP();
+		// $mail->Host = "smtp.gmail.com";
+		
+		// //Enable SMTP debugging
+		// // 0 = off (for production use)
+		// // 1 = client messages
+		// // 2 = client and server messages
+		// $mail->SMTPDebug = 2;
+		// //Ask for HTML-friendly debug output
+		// $mail->Debugoutput = 'html';
+		
+		// //Set the encryption system to use - ssl (deprecated) or tls
+		// $mail->SMTPSecure = 'tls';
+		
+		// // optional
+		// // used only when SMTP requires authentication  
+		// $mail->SMTPAuth = true;
+		// $mail->Username = 'shoetaku97@gmail.com';
+		// $mail->Password = 'shoetaku123';
+		
+		// $mail->setFrom("from@example.com");
+		// $mail->addAddress($_POST['email']);
+		// $mail->Subject = "Subject";
+		// $mail->Body = "Body";
+		
+		// if(!$mail->send()){
+		// 	echo "Message could not be sent";
+		// 	echo 'Mailer Error: ' . $mail->ErrorInfo;
+		// }else{
+		// 	echo 'Message sent';
+		// }
+		
+		ini_set("SMTP","ssl://smtp.gmail.com");
+		ini_set("smtp_port","587");
+		
+		$to = $_POST['email'];
+		$subject = "Subject";
+		$txt = 'test';
+		$headers = "From yo mama";
+		mail($to,$subject,$txt,$headers);
+		echo true;
 	}
 	
 	
