adde6ed693a80650b1cf76210c4b3a5c7587a71c
peterysc <peterysc@gmail.com>
2016-09-10 17:46:45 -0700

    pagination button fully functional for sell/wanted

--- partials/pagination-wanted.js
+++ partials/pagination-wanted.js
@@ -50,18 +50,29 @@ function renderPagination(page){
 				}					
 			});
 
-			pagination += '<div class="cell_disabled"><span>First</span></div><div class="cell_disabled"><span>Previous</span></div>';
-			for(i=1; i < (totalNumPage+1); i++){
-				pagination += '<div';
-				if(i==page){
-					pagination += ' class="cell_active"><span>' + i + '</span>';
-				}else{
-					pagination += ' class="cell"><a href="#" id="' + i + '">' + i + '</a>';
-					pagination += '</div>';
-				}
-			}
-			pagination += '<div><span>Next</span></div><div><span>Last</span></div>';
-			$('#listofPages').html(pagination); // We update the pagination DIV
+			var pagination = '';
+
+		    if (page == 1){
+		        pagination += '<li class="disabled"><a href="#" aria-label="First"<span aria-hidden="true">First</span></a></li>';
+		        pagination += '<li class="disabled><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>';
+		    }
+		    else {
+		        pagination += '<li><a href="#" aria-label="First" id="1"><span aria-hidden="true">First</span></a></li><li><a href="#" aria-label="Previous" id="' + (page - 1) + '">&laquo</span></a></li>';
+		    }
+
+		    for (var i=parseInt(page)-3; i<=parseInt(page)+3; i++) {
+		        if (i >= 1 && i <= totalNumPage) {
+		            pagination += '<li';
+		            if (i == page) pagination += ' class="active"><span>' + i + '</span>';
+		            else pagination += '><a href="#" id="' + i + '">' + i + '</a>';
+		            pagination += '</li>';
+		        }
+		    }
+
+		    if (page == totalNumPage) pagination += '<li class="disabled><a href="#" aria-label="Next"><span aria-hidden="true">&raquo</span></a></li><li class="disabled"><a href="#" aria-label="Last"<span aria-hidden="true">Last</span></a></li>';
+		    else pagination += '<li><a href="#" aria-label="Next" id="' + (parseInt(page) + 1) + '">&raquo</a></li><li><a href="#" aria-label="Last" id="' + totalNumPage + '">Last</span></a></li>';
+
+		    $('#listofPages').html(pagination); // We update the pagination DIV
 
 		},
 		error: function(data){
@@ -127,18 +138,29 @@ function bringFirstWantedPage(){
 				}					
 			});
 
-			pagination += '<div class="cell_disabled"><span>First</span></div><div class="cell_disabled"><span>Previous</span></div>';
-			for(i=1; i < (totalNumPage+1); i++){
-				pagination += '<div';
-				if(i==page){
-					pagination += ' class="cell_active"><span>' + i + '</span>';
-				}else{
-					pagination += ' class="cell"><a href="#" id="' + i + '">' + i + '</a>';
-					pagination += '</div>';
-				}
-			}
-			pagination += '<div><span>Next</span></div><div><span>Last</span></div>';
-			$('#listofPages').html(pagination); // We update the pagination DIV
+			var pagination = '';
+
+		    if (page == 1){
+		        pagination += '<li class="disabled"><a href="#" aria-label="First"<span aria-hidden="true">First</span></a></li>';
+		        pagination += '<li class="disabled><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>';
+		    }
+		    else {
+		        pagination += '<li><a href="#" aria-label="First" id="1"><span aria-hidden="true">First</span></a></li><li><a href="#" aria-label="Previous" id="' + (page - 1) + '">&laquo</span></a></li>';
+		    }
+
+		    for (var i=parseInt(page)-3; i<=parseInt(page)+3; i++) {
+		        if (i >= 1 && i <= totalNumPage) {
+		            pagination += '<li';
+		            if (i == page) pagination += ' class="active"><span>' + i + '</span>';
+		            else pagination += '><a href="#" id="' + i + '">' + i + '</a>';
+		            pagination += '</li>';
+		        }
+		    }
+
+		    if (page == totalNumPage) pagination += '<li class="disabled><a href="#" aria-label="Next"><span aria-hidden="true">&raquo</span></a></li><li class="disabled"><a href="#" aria-label="Last"<span aria-hidden="true">Last</span></a></li>';
+		    else pagination += '<li><a href="#" aria-label="Next" id="' + (parseInt(page) + 1) + '">&raquo</a></li><li><a href="#" aria-label="Last" id="' + totalNumPage + '">Last</span></a></li>';
+
+		    $('#listofPages').html(pagination); // We update the pagination DIV
 
 		},
 		error: function(data){
